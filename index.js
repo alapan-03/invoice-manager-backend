19d44dc37db55bef71043d48df43831605f6ca2b
alapan-03 <kalx.alapankar.1748395@gmail.com>
2024-11-24 21:59:00 +0530

    Fix/fixed cors origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: "http://localhost:3001",
+  origin: "https://invoice-manager-frontend.onrender.com/",
   optionsSuccessStatus: 200,
 };
 
