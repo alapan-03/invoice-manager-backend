8423815c330f235ecf763a97e59ee318629f403f
alapan-03 <kalx.alapankar.1748395@gmail.com>
2025-01-03 21:38:13 +0530

    Allowed cors from all origins

--- index.js
+++ index.js
@@ -13,13 +13,21 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const corsOptions = {
-  origin: "https://invoice-manager-frontend.onrender.com",
-  // origin: "http://localhost:3001",
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   // origin: "https://invoice-manager-frontend.onrender.com",
+//   origin: "http://localhost:3001",
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 
 // Configure Multer for file uploads
 const upload = multer({
